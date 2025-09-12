116e23e5769aa3052377c84c180c7d64db285894
Lalitha791 <lalithabhashini@gmail.com>
2025-09-12 19:18:04 +0530

    Add files via upload

---
+++ datatype.ts
@@ -0,0 +1,16 @@
+// Type Annotation
+let age: number = 21;
+let name: string = "Lekhaa";
+
+// Type Inference
+let city = "Hyderabad"; // inferred as string
+
+// Tuple
+let person: [string, number] = ["Anshu", 22];
+
+// Enum
+enum Direction { Up, Down, Left, Right }
+let move: Direction = Direction.Left;
+
+// Union
+let id: string | number = 101;
