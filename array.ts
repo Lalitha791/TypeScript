b8c1c9293ce678e668169c32f6bcfd5fe2745bef
Lalitha791 <lalithabhashini@gmail.com>
2025-09-17 19:38:53 +0530

    Create array.ts

---
+++ array.ts
@@ -0,0 +1,23 @@
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+
+
+var names: string[] = ["John", "Jane", "Jim", "Jill"];
+
+printNames();
+
+/**
+ * Print names
+ */
+function printNames() {
+    var name = prompt("Enter a name: ");
+    if (name === "exit") {
+        console.log(names);
+    }
+    else {
+        names.push(name);
+        printNames();
+    }
+}
